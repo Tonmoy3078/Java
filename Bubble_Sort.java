6ee06029974a22d8b6af3847ab8b3a36130d66a5
Tonmoy3078 <tkgaming3078@gmail.com>
2026-08-15 23:36:27 +0600

    first

---
+++ Bubble_Sort.java
@@ -0,0 +1,28 @@
+public class Bubble_Sort {
+    public static void main(String args[])
+    {
+
+        int arr[] = {7,8,5,6,3,4};
+
+        //bubble sort
+        //time complexity = O(n^2);
+        for(int i=0; i<arr.length-1; i++)
+        {
+            for(int j=0; j<arr.length-i-1; j++)
+            {
+                if(arr[j]>arr[j+1])
+                {
+                    //swap
+                    int t = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1]=t;
+                }
+            }
+        }
+
+        for(int i=0; i<arr.length; i++)
+        {
+            System.out.println(arr[i]);
+        }
+    }
+}
--- tonmoy.java
+++ tonmoy.java
@@ -1,31 +1,20 @@
+import java.text.DecimalFormat;
 import java.util.*;
 
-public class tonmoy
-{
- 
-  public static void main(String args[])
+public class tonmoy {
+    public static void main(String args[]) 
     {
-       StringBuilder sb = new StringBuilder("tonmoy");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
-       char sp =  sb.charAt(0);
-       System.out.println( sp);
-
-       sb.insert(0, "tk");
-       System.out.println(sb);
-
-       sb.reverse();
-       System.out.println(sb);
-
-       sb.delete(0, 3);
-       System.out.println(sb);
-
-       sb.append("das");
-       System.out.println(sb);
-      
-       System.out.println(sb.length());
-
-
-
+        int a=1,b=1,c=1;
+        for(int i=1; i<=n; i++)
+        {
+          System.out.println(a+" "+b+" "+c); 
+          b=a+a;
+          c=a*b;
+          System.out.println(a+" "+b+" "+a*b);
+          a++;
+        }
     }
-  }
-
+}
